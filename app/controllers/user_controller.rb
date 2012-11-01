01ad54ec79ba421491e3016050ecfff8a40f2f4b
Shilpa Apte <sapte1@stanford.edu>
2012-10-31 20:36:30 -0700

    changed css

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,14 +4,16 @@ class UserController < ApplicationController
 	end
     
     def profile
-        
+        @default_user = ["Shilpa", "CS 147", "Meat on the Bones!", "CS 221", "No status available"]
     end
 
     def favs
-
+    	@default_favorite = ["George", "CS 147", "Meat on the Bones!", "CS 221", "I die."]
+    	@default_user = ["Shilpa", "CS 147", "Meat on the Bones!", "CS 221", "No status available"]
 	end
     
     def map
-        
+    	@default_my_pin = ["Shilpa", 37.4419, 122.1419]
+        @default_friend_pin = ["George", 37.4225, 122.1653]
     end
 end
