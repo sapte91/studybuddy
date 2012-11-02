53ad078e1510ea6404d48b2c8303fdf736e98cf3
George Michopoulos <giorgos@stanford.edu>
2012-11-01 20:32:09 -0700

    fixed password functionality and notice capabilities and css

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,6 +4,35 @@ class UserController < ApplicationController
 	def login
         @curr_username = "";
 	end
+
+    def post_login
+        if params[:login] == "George" and params[:pw] == "test"
+            @curr_username = "George"
+            redirect_to :action => 'map'
+            flash[:notice] = "Welcome to StudyBuddy, " + @curr_username + "!"
+        else 
+            redirect_to :action => 'login'
+            flash[:notice] = "Incorrect User/Password Combination."
+        end
+    end
+
+=begin
+        if !user.nil? 
+            if user.password_valid?(params[:pw])                
+                session[:curr_user] = user.id
+                redirect_to :controller => 'pics', :action => 'user', :id => user.id
+                flash[:notice] = "Welcome to Papparazzzi, " + user.first_name + "."
+            else
+                redirect_to :controller => 'user', :action => 'login'
+                flash[:notice] = "Incorrect Password."
+            end
+        else
+            redirect_to :controller => 'user', :action => 'login'
+            flash[:notice] = "No such user; please try a different login."
+        end
+    end
+=end
+
     
     def profile
         @curr_user = ["Shilpa", [["CS 147", "Meat on the Bones!", 2], ["CS 221", "No status available", 3]]]
